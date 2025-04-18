cc9fa25c406a5ae966d46f8296face18cc3462e8
NghiaLee <lenghiadev81@gmail.com>
2025-04-18 10:43:20 +0700

    complete update

--- Admin_UI/src/pages/thongKeChiTiet.jsx
+++ Admin_UI/src/pages/thongKeChiTiet.jsx
@@ -35,36 +35,7 @@ const ThongKeChiTiet = () => {
         THỐNG KÊ CHI TIẾT
       </Typography>
 
-      <StyleBackground>
-        <Box>
-          <Typography variant="button">THỐNG KÊ THEO KHU VỰC</Typography>
-          <Box sx={{ margin: '10px 0px' }}>
-            <SelectArea
-              onChange={(value) => {
-                console.log('Khu vực đã chọn:', value);
-                setArea(value);
-              }}
-            />
-          </Box>
-
-          <Grid container direction="row" alignItems="center" justifyContent="space-between">
-            <Grid item xs={12} md={6} spacing={3}>
-              <CustomDatePicker label="Ngày bắt đầu" value={from} onChange={setFrom} />
-              <CustomDatePicker label="Ngày kết thúc" value={to} onChange={setTo} />
-            </Grid>
-            <Grid item xs={12} md={2} display="flex" justifyContent="center">
-              <DownloadButtons onClick={handleDownload} />
-            </Grid>
-          </Grid>
-        </Box>
-        <Box sx={{ marginTop: '10px', padding: 2 }}>
-          {data.length === 0 ? (
-            <Typography>Chưa có dữ liệu, vui lòng tải dữ liệu.</Typography>
-          ) : (
-            <ChartComponent from={from} to={to} data={data} />
-          )}
-        </Box>
-      </StyleBackground>
+      <StyleBackground></StyleBackground>
     </Box>
   );
 };
