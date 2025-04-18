b57336746cfc9d43a73fac9d912ddbcfee5b37eb
NghiaLee <lenghiadev81@gmail.com>
2025-04-18 16:55:52 +0700

    update linechar and update button addArea

--- Admin_UI/src/components/button/addArea.jsx
+++ Admin_UI/src/components/button/addArea.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Button } from '@mui/material';
-const AddArea = ({ onClick }) => {
+import { useState } from 'react';
+import { Button, Dialog } from '@mui/material';
+import AddAreaContent from '../addArea/addAreaContent';
+const AddArea = () => {
+  const [open, setOpen] = useState(false);
+  const handOpend = () => setOpen(true);
+  const handClose = () => setOpen(false);
   return (
     <div>
-      <Button variant="contained" onClick={onClick} sx={{ width: 150 }}>
+      <Button variant="contained" onClick={handOpend} sx={{ width: 150 }}>
         Thêm khu vực
       </Button>
+      <Dialog open={open} onClose={handClose}>
+        <AddAreaContent onClose={handClose} />
+      </Dialog>
     </div>
   );
 };
