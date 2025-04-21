8995c500fe048a67dbd4caa357efacd9eef4f8df
vynhatduy <vynhatduy111@gmail.com>
2025-04-21 09:03:03 +0700

    create logic add area

--- Admin_UI/src/components/button/addArea.jsx
+++ Admin_UI/src/components/button/addArea.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Dialog } from '@mui/material';
 import AddAreaContent from '../addArea/addAreaContent';
+import { useCreateArea } from '../../service/useArea';
+
 const AddArea = () => {
   const [open, setOpen] = useState(false);
-  const handOpend = () => setOpen(true);
-  const handClose = () => setOpen(false);
+  const { createArea, loading, error, success } = useCreateArea();
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleSubmit = async (payload) => {
+    payload.id = '';
+    console.log('payload', payload);
+    const result = await createArea(payload);
+    if (result) {
+      console.log('Khu vực đã được tạo:', result);
+      handleClose();
+    }
+  };
+
   return (
     <div>
-      <Button variant="contained" onClick={handOpend} sx={{ width: 150 }}>
+      <Button variant="contained" onClick={handleOpen} sx={{ width: 150 }}>
         Thêm khu vực
       </Button>
-      <Dialog open={open} onClose={handClose}>
-        <AddAreaContent onClose={handClose} />
+      <Dialog open={open} onClose={handleClose}>
+        <AddAreaContent onClose={handleClose} onSubmit={handleSubmit} />
       </Dialog>
     </div>
   );
